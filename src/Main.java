89b5303f9c1203594662e76af88ee8a01cee3042
việt Ân <luudinhvietan1123@gmail.com>
2020-11-29 19:58:06 +0700

    update thread

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         //test case 1: 50 luồng: con trỏ duyệt loop 50 lần nhanh hơn nên các luồng đc tạo 1 lượt trước khi luồng chạy
         //test case 2: 5000 luồng: sự khác biệt rõ ràng khi con trỏ giờ phải đáp ứng nhu cầu tạo số lượng luồng lớn
-        for(int i=0; i<50; i++){
+        for(int i=0; i<10; i++){
             String newThreadName = String.valueOf(i);
             NewThread newThread = new NewThread(newThreadName);
         }
--- src/NewThread.java
+++ src/NewThread.java
@@ -11,7 +11,11 @@ public class NewThread implements Runnable{
 
     public void run(){
         System.out.println("Thread " + name + " is running...");
+        while(true){
+        }
     }
+    //While true de thread luon chay
+
     // Tại sao thread sau khi được tạo không start luôn mà phải đợi tạo hết các thread mới start(trong trường hợp của bài 50 luồng)???
     // ->> tốc độ của con trỏ chạy qua các luồng (trong trường hợp 50 luồng - khá bé, thử tăng số lượng thread lên > 500 sẽ thấy rõ)
     // https://voer.edu.vn/c/luong/a22667db/766d6175
